aed8f43cfeb210cc3c4d1c2e7935a7fd0abd1454
nabinpaudel-np <linktonabin@gmail.com>
2021-08-03 21:16:43 +0545

    Fetch Function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 let weather = {
-     apiKey: "73d7a52ff917b3129031a62c0234e8eb",
+     apiKey: "9de75284016a6cecfeaa66d0f3fef268",
      fetchWeather: function (city) {
           fetch(
-               "http://api.openweathermap.org/data/2.5/weather?q= " 
-               + city 
-               + "&units=metric&appid= " 
-               + this.apiKey
+               "https://api.openweathermap.org/data/2.5/weather?q=" 
+                + city 
+                +"&units=metric&appid=" 
+                + this.apiKey
           )
-               .then((response) => response.json())
-               .then((data) => console.log(data));
-     },
-     displayWeather: function(data) {
-          const { name } = {data};
-          const { icon, description } = data.weather;
+          .then((response) => response.json())
+               .then((data) => this.displayWeather(data));
 
+          },
+     displayWeather: function(data) {
+          const { name } = data;
+          const { icon, description } = data.weather[0];
+          const { temp, humidity } = data.main; 
+          const { speed } = data.wind;
+          console.log(name, icon, description, temp, humidity, speed)
+          document.querySelector(".city").innerHTML = "Weather in "   + name;
+          document.querySelector(".temp").innerHTML = temp + "&#176;C";
+          document.querySelector(".icon").src = 
+          "https://openweathermap.org/img/wn/"+ icon +".png";
+
+          document.querySelector(".description").innerHTML = description;
+          document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+          document.querySelector(".wind").innerHTML = "Wind Speed: "+ speed + "km/h";
 
      }
 };
