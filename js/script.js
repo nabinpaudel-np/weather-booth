71448a29a48685b825cb336969ce06660df8b34d
nabinpaudel-np <linktonabin@gmail.com>
2021-08-05 16:44:54 +0545

    Design Change

--- js/script.js
+++ js/script.js
@@ -9,23 +9,39 @@ let weather = {
           )
           .then((response) => response.json())
                .then((data) => this.displayWeather(data));
-
           },
      displayWeather: function(data) {
           const { name } = data;
           const { icon, description } = data.weather[0];
           const { temp, humidity } = data.main; 
           const { speed } = data.wind;
-          console.log(name, icon, description, temp, humidity, speed)
           document.querySelector(".city").innerHTML = "Weather in "   + name;
           document.querySelector(".temp").innerHTML = temp + "&#176;C";
           document.querySelector(".icon").src = 
-          "https://openweathermap.org/img/wn/"+ icon +".png";
-
+               "https://openweathermap.org/img/wn/"+ icon +".png";
           document.querySelector(".description").innerHTML = description;
           document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
           document.querySelector(".wind").innerHTML = "Wind Speed: "+ speed + "km/h";
+          document.querySelector(".weather").classList.remove("loading");
+          document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?nature,water')"
+     },
 
+
+
+     search: function() {
+          this.fetchWeather(document.querySelector(".search-bar").value);
      }
 };
 
+document.querySelector(".search button").addEventListener("click", function(){
+     weather.search();
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+     if (event.key == "Enter") {
+          weather.search(); 
+     }
+})
+
+
+weather.fetchWeather("Sanepa");
